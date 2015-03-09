772ec232f300322d5a5034efb10ba6477db9a09b
tfrom222 <tfrom222@gmail.com>
2015-03-09 19:21:13 +0100

    Added Auto Zooming Camera
    I added an auto zooming camera based of all players positions so you can
    always see every player.

---
+++ Assets/SmashCam.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class SmashCam : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform[] targets;
+
+    [SerializeField]
+    float boundingBoxPadding = 2f;
+
+    [SerializeField]
+    float minimumOrthographicSize = 8f;
+
+    [SerializeField]
+    float zoomSpeed = 20f;
+
+    Camera camera;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+        camera.orthographic = true;
+    }
+
+    void LateUpdate()
+    {
+        Rect boundingBox = CalculateTargetsBoundingBox();
+        transform.position = CalculateCameraPosition(boundingBox);
+        camera.orthographicSize = CalculateOrthographicSize(boundingBox);
+    }
+
+    /// Calculates a bounding box that contains all the targets.
+
+    /// Returns A Rect containing all the targets
+    Rect CalculateTargetsBoundingBox()
+    {
+        float minX = Mathf.Infinity;
+        float maxX = Mathf.NegativeInfinity;
+        float minY = Mathf.Infinity;
+        float maxY = Mathf.NegativeInfinity;
+
+        foreach (Transform target in targets)
+        {
+            Vector3 position = target.position;
+
+            minX = Mathf.Min(minX, position.x);
+            minY = Mathf.Min(minY, position.y);
+            maxX = Mathf.Max(maxX, position.x);
+            maxY = Mathf.Max(maxY, position.y);
+        }
+
+        return Rect.MinMaxRect(minX - boundingBoxPadding, maxY + boundingBoxPadding, maxX + boundingBoxPadding, minY - boundingBoxPadding);
+    }
+
+
+    /// Calculates a camera position given the a bounding box containing all the targets.
+
+    Vector3 CalculateCameraPosition(Rect boundingBox)
+    {
+        Vector2 boundingBoxCenter = boundingBox.center;
+
+        return new Vector3(boundingBoxCenter.x, boundingBoxCenter.y, camera.transform.position.z);
+    }
+
+   
+    /// Calculates a new orthographic size for the camera based on the target bounding box.
+
+    float CalculateOrthographicSize(Rect boundingBox)
+    {
+        float orthographicSize = camera.orthographicSize;
+        Vector3 topRight = new Vector3(boundingBox.x + boundingBox.width, boundingBox.y, 0f);
+        Vector3 topRightAsViewport = camera.WorldToViewportPoint(topRight);
+
+        if (topRightAsViewport.x >= topRightAsViewport.y)
+            orthographicSize = Mathf.Abs(boundingBox.width) / camera.aspect / 2f;
+        else
+            orthographicSize = Mathf.Abs(boundingBox.height) / 2f;
+
+        return Mathf.Clamp(Mathf.Lerp(camera.orthographicSize, orthographicSize, Time.deltaTime * zoomSpeed), minimumOrthographicSize, Mathf.Infinity);
+    }
+}
