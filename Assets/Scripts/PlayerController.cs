29492241f3e8c7f74a69408602d1f875eaff179f
Jaggler3 <martin.protostar@gmail.com>
2015-03-17 21:09:46 -0400

    Added Left and Right Controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator animator;
+
+    public bool normalControl = true;
+
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        animator = transform.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (normalControl)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+
+                transform.position += transform.forward * Time.deltaTime * speed;
+
+                animator.SetBool("Forward", true);
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                animator.SetBool("Forward", false);
+
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+
+                transform.position += transform.forward * Time.deltaTime * speed;
+
+                animator.SetBool("Forward", true);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                animator.SetBool("Forward", false);
+            }
+        }
+    }
+
+}
