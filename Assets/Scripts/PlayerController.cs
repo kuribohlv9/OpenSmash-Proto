659dc4f55cc262f4721d358117f883c7e04f432a
Jaggler3 <martin.protostar@gmail.com>
2015-03-18 21:15:50 -0400

    Collision layers and constant gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,20 @@ public class PlayerController : MonoBehaviour {
 
     public Animator m_Animator;
 
+    public Rigidbody m_Rigidbody;
+
     public bool m_NormalControl = true;
 
     public float m_MovementSpeed = 1.0f;
+
+    public float m_Gravity = 1.0f;
 
     public KeyCode m_LeftKey;
     public KeyCode m_RightKey;
 
     void Start() {
         if (m_Animator == null) m_Animator = transform.gameObject.GetComponent<Animator>();
+        if (m_Rigidbody == null) m_Rigidbody = transform.gameObject.GetComponent<Rigidbody>();
     }
 
     void Update() {
@@ -28,6 +33,9 @@ public class PlayerController : MonoBehaviour {
                 m_Animator.SetBool("Forward", false);
             }
         }
+
+        m_Rigidbody.velocity = new Vector3(0, -m_Gravity, 0);
+        
     }
 
 }
